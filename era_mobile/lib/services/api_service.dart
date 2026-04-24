31ba8d87e6f7a9fb2f69bbebe2b7954bab5d2547
Briana <brownbb1990@gmail.com>
2026-04-24 21:12:36 +0000

    image upload

--- era_mobile/lib/services/api_service.dart
+++ era_mobile/lib/services/api_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 
 const String baseUrl = 'https://improved-giggle-4qwv56rvwgrf9vg-8000.app.github.dev';
@@ -17,6 +18,7 @@ class ApiService {
     required String username,
     required String content,
     List<String> tags = const [],
+    String? mediaUrl,
   }) async {
     final response = await http.post(
       Uri.parse('$baseUrl/posts/'),
@@ -26,6 +28,7 @@ class ApiService {
         'username': username,
         'content': content,
         'tags': tags,
+        'media_url': mediaUrl,
       }),
     );
     if (response.statusCode == 200) {
@@ -36,5 +39,24 @@ class ApiService {
 
   static Future<void> likePost(int postId) async {
     await http.post(Uri.parse('$baseUrl/posts/$postId/like'));
+  }
+
+  static Future<String?> uploadImage(Uint8List bytes, String filename) async {
+    final request = http.MultipartRequest(
+      'POST',
+      Uri.parse('$baseUrl/media/upload/image'),
+    );
+    request.files.add(http.MultipartFile.fromBytes(
+      'file',
+      bytes,
+      filename: filename,
+    ));
+    final response = await request.send();
+    if (response.statusCode == 200) {
+      final body = await response.stream.bytesToString();
+      final data = jsonDecode(body);
+      return data['url'];
+    }
+    return null;
   }
 }

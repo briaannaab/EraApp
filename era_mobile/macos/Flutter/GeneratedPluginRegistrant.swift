da960f48758e93b6cdf2a7395f42212fa25229bd
Briana <brownbb1990@gmail.com>
2026-05-08 17:56:01 +0000

    livestream build

--- era_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ era_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,13 +5,17 @@
 import FlutterMacOS
 import Foundation
 
+import agora_rtc_engine
 import file_picker
+import iris_method_channel
 import package_info_plus
 import video_player_avfoundation
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AgoraRtcNgPlugin.register(with: registry.registrar(forPlugin: "AgoraRtcNgPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  IrisMethodChannelPlugin.register(with: registry.registrar(forPlugin: "IrisMethodChannelPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))

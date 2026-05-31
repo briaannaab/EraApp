b8a8b1ee3234a7c3963a49e8b981356866204c79
Briana <brownbb1990@gmail.com>
2026-05-31 17:58:19 +0000

    Add image compression before upload

--- era_mobile/lib/services/api_service.dart
+++ era_mobile/lib/services/api_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:typed_data';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:http/http.dart' as http;
 
 const String baseUrl = 'https://eraapp-production.up.railway.app';
